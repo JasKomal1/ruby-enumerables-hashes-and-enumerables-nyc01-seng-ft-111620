380a1df524a7eae0102c2f7d567e082c7ca07257
jsr <jaskomal2@gmail.com>
2020-11-02 22:19:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,8 +5,9 @@
 # suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
 # }
-winner = ""
+
 def select_winner(passengers)
+  winner = ""
   passengers.each do |key,value| 
       if key == :suite_a && value.start_with?("A") 
         winner = value  
